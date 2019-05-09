c4a80e1f014ac0f7c638cc87624d298c0112f53c
Anthony Hughes <tony2nite@gmail.com>
2019-05-09 07:46:45 +0200

    Hello Everyone! Our second commit

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// start red section
+
+var red = "red";
+
+// end red section
+
+// start blue section;
+
+var blue = "blue";
+
+// end blue section
+
+// start pink section;
+
+var pink = "pink";
+
+// end pink section
+
+// start green section;
+
+var green = "green";
+
+// end green section
+
+// start purple section;
+
+var purple = "purple";
+
+// end purple section
